d46bcee39aab67ac96dab342330754e6b107c790
Charlotte Lewis <charlotte.lewis12@gmail.com>
2021-03-25 15:53:04 -0400

    lesson 5

--- lesson_5/practice_problems.rb
+++ lesson_5/practice_problems.rb
@@ -126,3 +126,54 @@ hsh = {}
 arr.each do |item|
   hsh[item[0]] = item[1]
 end
+
+# Q13
+arr = [[1, 6, 7], [1, 4, 9], [1, 8, 3]]
+
+arr.sort_by do |sub|
+  sub.select { |element| element.odd? }
+end
+
+# Q14
+  # Given this data structure write some code to return an array containing
+  # the colors of the fruits and the sizes of the vegetables.
+  # The sizes should be uppercase and the colors should be capitalized.
+
+  # [["Red", "Green"], "MEDIUM", ["Red", "Green"], ["Orange"], "LARGE"]
+  hsh = {
+    'grape' => {type: 'fruit', colors: ['red', 'green'], size: 'small'},
+    'carrot' => {type: 'vegetable', colors: ['orange'], size: 'medium'},
+    'apple' => {type: 'fruit', colors: ['red', 'green'], size: 'medium'},
+    'apricot' => {type: 'fruit', colors: ['orange'], size: 'medium'},
+    'marrow' => {type: 'vegetable', colors: ['green'], size: 'large'},
+  }
+  arr = []
+
+  hsh.map do |_, attributes|
+    case attributes[:type]
+    when 'fruit'
+      then attributes[:colors].map { |color| color.capitalize }
+    when 'vegetable'
+      then
+      attributes[:size].upcase
+    end
+  end
+
+# Q15
+arr = [{a: [1, 2, 3]}, {b: [2, 4, 6], c: [3, 6], d: [4]}, {e: [8], f: [6, 10]}]
+arr.select do |hsh|
+  hsh.values.flatten.all? { |int| int.even? }
+end
+
+# Q16
+# 8-4-4-4-12
+# "f65c57f6-a6aa-17a8-faa1-a67f2dc9fa91"
+
+def generate_uuid()
+  chars = ('a'..'z').to_a + ('1'..'9').to_a
+  sections = [8, 4, 4, 4, 12]
+  uuid = []
+
+  sections.each { |num| uuid << chars.sample(num).join }
+  uuid.join('-')
+end
