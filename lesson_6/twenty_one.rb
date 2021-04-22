7ca5b364d9f9db266234e469052416c83ed08114
EC2 Default User <ec2-user@ip-172-31-22-55.us-east-2.compute.internal>
2021-04-22 16:06:56 +0000

    Moved result messages to yaml file

--- lesson_6/twenty_one.rb
+++ lesson_6/twenty_one.rb
@@ -1,7 +1,6 @@
 require 'yaml'
 MESSAGES = YAML.load_file('twenty_one_messages.yml')
 
-
 SUITS = %w(Hearts Clubs Diamonds Spades)
 VALUES = ('2'..'10').to_a + %w(Jack Queen King Ace)
 DIVIDER = "=============="
@@ -26,22 +25,22 @@ end
 
 def start_game
   prompt(MESSAGES['welcome'])
-  
+
   display_rules = get_valid_input(MESSAGES['request_rules'], 'y', 'n')
   prompt(MESSAGES['rules']) if display_rules == 'y'
-  
+
   prompt(MESSAGES['start'])
   gets
+  system 'clear'
 end
 
 def initialize_deck
-  deck = SUITS.each_with_object([]) do |suit, deck|
-    VALUES.each { |value| deck << {suit: suit, value: value} }
+  deck = SUITS.each_with_object([]) do |suit, array|
+    VALUES.each { |value| array << { suit: suit, value: value } }
   end
 
   deck.shuffle
 end
-
 
 def join_and(array)
   return array[0] if array.size == 1
@@ -55,7 +54,7 @@ end
 
 def total(hand)
   score = 0
-  
+
   card_values = hand.map { |card| card[:value] }
   card_values.each do |value|
     if %w(King Queen Jack).include?(value)
@@ -69,7 +68,7 @@ def total(hand)
   card_values.select { |value| value == 'Ace' }.count.times do
     score <= 10 ? (score += 11) : (score += 1)
   end
-  
+
   score
 end
 
@@ -93,26 +92,15 @@ end
 
 def display_result(totals)
   result = detect_result(totals)
-
-  case result
-  when :player_busted
-    prompt("You busted! Dealer wins!")
-  when :dealer_busted
-    prompt("Dealer busted! You win!")
-  when :player_won
-    prompt("You win!")
-  when :dealer_won
-    prompt("Dealer wins!")
-  when :tie
-    prompt("It's a tie!")
-  end
+  prompt(MESSAGES[result.to_s])
 end
 
 def play_again?
-  answer = get_valid_input("Do you want to play another round? Enter Yes or No", 'y', 'n')
+  answer = get_valid_input(MESSAGES['play_again'], 'y', 'n')
   answer == 'y'
 end
 
+# rubocop:disable  Metrics/AbcSize
 def end_round(dealer_cards, player_cards, totals, wins)
   sleep(0.5)
   puts DIVIDER
@@ -120,19 +108,20 @@ def end_round(dealer_cards, player_cards, totals, wins)
          "for a total of: #{totals[:dealer]}")
   prompt("You have #{format_hand(player_cards)}, "\
          "for a total of: #{totals[:player]}")
-  
+
   sleep(1)
   puts DIVIDER
   display_result(totals)
-  
+
   sleep(0.5)
   puts DIVIDER
   prompt("Dealer has won #{wins[:dealer]} games")
   prompt("You have won #{wins[:player]} games")
-  
+
   puts DIVIDER
   sleep(0.5)
 end
+# rubocop:enable  Metrics/AbcSize
 
 def update_wins!(wins, totals)
   result = detect_result(totals)
@@ -159,31 +148,26 @@ def player_hit(deck, player_cards, totals)
 
   prompt("You chose to hit!")
   sleep(0.75)
-  
+
   return if busted?(totals[:player])
   prompt("Your cards are now: #{format_hand(player_cards)}")
   prompt("Your total is now: #{totals[:player]}")
 end
 
-def player_turn(deck, dealer_cards, player_cards, totals, wins)
-   # Player Turn
+def player_turn(deck, player_cards, totals)
+  # Player Turn
   player_action = nil
   loop do
     player_action = get_valid_input("hit or stay?", 'h', 's')
     system 'clear'
-    
+
     player_hit(deck, player_cards, totals) if player_action == 'h'
     break if busted?(totals[:player]) || player_action == 's'
   end
   sleep(0.5)
 end
 
-def dealer_turn(deck, dealer_cards, totals)
-  sleep(0.5)
-  prompt("Dealer's turn...")
-  sleep(0.5)
-  prompt("Dealer's cards are: #{format_hand(dealer_cards)}")
-  prompt("Dealer's total is: #{totals[:dealer]}")
+def dealer_hit(deck, dealer_cards, totals)
   loop do
     sleep(0.5)
     break if totals[:dealer] >= DEALER_MAX || busted?(totals[:dealer])
@@ -196,6 +180,17 @@ def dealer_turn(deck, dealer_cards, totals)
     prompt("Dealer's cards are now: #{format_hand(dealer_cards)}")
     prompt("Dealer's total is now: #{totals[:dealer]}")
   end
+end
+
+def dealer_turn(deck, dealer_cards, totals)
+  sleep(0.5)
+  prompt("Dealer's turn...")
+
+  sleep(0.5)
+  prompt("Dealer's cards are: #{format_hand(dealer_cards)}")
+  prompt("Dealer's total is: #{totals[:dealer]}")
+
+  dealer_hit(deck, dealer_cards, totals)
 
   sleep(0.5)
   prompt("Dealer stays at #{totals[:dealer]}") if !busted?(totals[:dealer])
@@ -205,7 +200,6 @@ system 'clear'
 start_game
 prompt("Dealing cards...")
 sleep(1)
-system 'clear'
 
 wins = {
   player: 0,
@@ -213,6 +207,8 @@ wins = {
 }
 
 loop do
+  system 'clear'
+  
   deck = initialize_deck
   player_cards = deck.pop(2)
   dealer_cards = deck.pop(2)
@@ -225,14 +221,14 @@ loop do
   prompt("Dealer has: #{format_hand([dealer_cards[0]])} and unknown card")
   prompt("You have: #{format_hand(player_cards)}, "\
          "for a total of: #{totals[:player]}")
-         
-  player_turn(deck, dealer_cards, player_cards, totals, wins)
+
+  player_turn(deck, player_cards, totals)
   if busted?(totals[:player])
     update_wins!(wins, totals)
     end_round(dealer_cards, player_cards, totals, wins)
 
     break if wins.values.max >= ROUND_WINS
-    
+
     sleep(0.5)
     play_again? ? next : break
   else
@@ -241,9 +237,9 @@ loop do
     prompt("You stayed at #{totals[:player]}")
     puts DIVIDER
   end
-  
+
   dealer_turn(deck, dealer_cards, totals)
-  
+
   update_wins!(wins, totals)
   end_round(dealer_cards, player_cards, totals, wins)
 
@@ -253,4 +249,4 @@ loop do
 end
 
 display_game_winner(wins)
-prompt("Thanks for playing! Goodbye.")
+prompt(MESSAGES['goodbye'])
